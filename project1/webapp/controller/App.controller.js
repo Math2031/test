6fa3593fd3207f40e321875ab8c2a9a2b15a073c
Mathieu Orma <mathieuorma62@gmail.com>
2023-10-27 12:04:40 +0000

    week2 u2

--- project1/webapp/controller/App.controller.js
+++ project1/webapp/controller/App.controller.js
@@ -1,15 +1,21 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/base/Log"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller,Log) {
         "use strict";
 
         return Controller.extend("opensap.project1.controller.App", {
             onInit: function () {
-
+                Log.info("Init");
+            },
+            onPress: function (sValue) {
+                sap.ui.require(["sap/m/MessageToast"], function (oMessage) {
+                    oMessage.show("Searching..." + " " + sValue);
+                });
             }
         });
     });
