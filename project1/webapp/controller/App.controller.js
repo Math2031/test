6bd999e0484bc154049df61851de34d6e75a8456
Mathieu Orma <mathieuorma62@gmail.com>
2023-10-30 14:08:10 +0000

    week2 u3

--- project1/webapp/controller/App.controller.js
+++ project1/webapp/controller/App.controller.js
@@ -1,14 +1,19 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/base/Log"
+    "sap/base/Log",
+    "../model/formatter",
+    "sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller,Log) {
+    function (Controller,Log, formatter,Filter, FilterOperator) {
         "use strict";
 
         return Controller.extend("opensap.project1.controller.App", {
+            formatter: formatter,
+
             onInit: function () {
                 Log.info("Init");
             },
@@ -16,6 +21,14 @@ sap.ui.define([
                 sap.ui.require(["sap/m/MessageToast"], function (oMessage) {
                     oMessage.show("Searching..." + " " + sValue);
                 });
+                let sCity = this.byId('city').getValue();
+                let sGenre = this.byId('genre').getSelectedKey();
+                let oFilterGenre = sGenre ? new Filter("genre", FilterOperator.EQ , sGenre) : null;
+                let oFilterCity = sCity ? new Filter("info", FilterOperator.Contains , sCity) : null;
+                this.byId('calendar').getBinding('rows').filter(oFilterGenre);
+                this.byId('calendar').getAggregation('rows').forEach( oItem => {
+                    oItem.getBinding('appointments').filter(oFilterCity);
+                })
             }
         });
     });
